b304c71357c018578ffaccc1dbf9b9bb157f34e9
Javi Carrion <suavejavi@google.com>
2019-04-17 17:13:37 -0400

    ⭐️ feat(model): Add addTodo method to add todos to the todos array

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,11 @@ const model = {
     todos: ['test1', 'test2', 'test3'],
     displayTodos() {
         console.log('Hey', this.todos);
+    },
+    addTodo(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
     }
 };
 
+model.addTodo('testOut');
