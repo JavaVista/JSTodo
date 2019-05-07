15aff824c9a46a264f1d8b2bb4b379025163efea
Javi Carrion <suavejavi@google.com>
2019-05-07 15:56:12 -0400

    ♻️refactor(model): Add ternary to show completed property

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,43 @@
 const model = {
-    todos: [],
-    displayTodos() {
-        this.todos.length === 0 ? console.log('List is empty') : console.log('My todos list:');
-        this.todos.forEach((todoContent, index) => {
-            console.log(index, todoContent.todo)
-        });
-    },
-    addTodo(todo) {
-        this.todos.push({
-            todo: todo,
-            completed: false
-        });
-        this.displayTodos();
-    },
-    changeTodo(index, todo) {
-        this.todos[index].todo = todo;
-        this.displayTodos();
-    },
-    deleteTodo(index) {
-        this.todos.splice(index, 1);
-        this.displayTodos();
-    },
-    toggleCompleted(index) {
+	todos: [],
+	displayTodos() {
+		this.todos.length === 0
+			? console.log('List is empty')
+			: console.log('My todos list:');
+		this.todos.forEach((todoContent, index) => {
+			// check if todo is completed
+			todoContent.completed
+				? console.log(`(X) ${todoContent.todo}`)
+				: console.log(`( ) ${todoContent.todo}`);
+		});
+	},
+	addTodo(todo) {
+		this.todos.push({
+			todo: todo,
+			completed: false,
+		});
+		this.displayTodos();
+	},
+	changeTodo(index, todo) {
+		this.todos[index].todo = todo;
+		this.displayTodos();
+	},
+	deleteTodo(index) {
+		this.todos.splice(index, 1);
+		this.displayTodos();
+	},
+	toggleCompleted(index) {
         const todo = this.todos[index];
-        todo.completed = !todo.completed;
-        this.displayTodos();
-    }
+		todo.completed = !todo.completed;
+		this.displayTodos();
+	},
 };
 
 // test
 model.displayTodos();
 model.addTodo('item 1');
 model.addTodo('item 2');
-model.deleteTodo(0);
-model.deleteTodo(0);
+model.toggleCompleted(0);
+model.toggleCompleted(1);
+model.toggleCompleted(1);
+
